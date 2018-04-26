c86eda5fb114cb0bd4ee5a7e5d3793423790be68
maumau35 <35198362+maumau35@users.noreply.github.com>
2018-04-26 09:07:07 +0200

    Update 2400gbot.py

--- 2400gbot.py
+++ 2400gbot.py
@@ -11,6 +11,15 @@ reddit=praw.Reddit(client_id='',
                    password='',
                    user_agent='')
 
+
+                        if comment.subreddit not in ["test","depression","suicidewatch"]:
+                            comment.body=comment.body.split("\n")
+                            print"replying to {0}'s (I'm lonely) comment: {1}".format(comment.author, comment.body)
+                            posts_repli_to.append(author.name)
+                            message=(""">I'm lonely
+
+
+
 done=['2400gbot']
 if not os.path.isfile("posts_repli_to.txt"):
     posts_repli_to = []
@@ -26,33 +35,39 @@ for comment in comments:
     author=comment.author
     try:
         if re.search("i'm lonely", comment.body, re.IGNORECASE):
-
-                if ('cat' not in comment.body and
-                    'dog' not in comment.body and
-                    'kitty' not in comment.body and
-                    'suicide' not in comment.body):
-                    if author.name in posts_repli_to:
-                        if author.name not in done:
-                            print "you have already commented on /u/%s before" % comment.author
-
+            if ('cat' not in comment.body and
+                'dog' not in comment.body and
+                'kitty' not in comment.body and
+                'suicide' not in comment.body):
+                if author.name in posts_repli_to:
+                    if author.name not in done:
+                        print "you have already commented on /u/%s before" % comment.author
 
 
-                    if author.name not in posts_repli_to and author.name not in done:
-                        if comment.subreddit not in ["test","depression","suicidewatch"]:
-                            comment.body=comment.body.split("\n")
-                            print"replying to {0}'s (I'm lonely) comment: {1}".format(comment.author, comment.body)
-                            posts_repli_to.append(author.name)
-                            message=(""">I'm lonely
+
+                if author.name not in posts_repli_to and author.name not in done:
+                    if comment.subreddit not in ["test","depression","suicidewatch"]:
+                        comment.body=comment.body.split("\n")
+                        print"replying to {0}'s (I'm lonely) comment: {1}".format(comment.author, comment.body)
+                        posts_repli_to.append(author.name)
+                        message=(""">I'm lonely
 
 
 
 Here are a few funny [dog pictures](https://imgur.com/a/XyHgX) for you /u/%s, to cheer you up!
 ___
 ^^Hello, ^^I'm ^^a ^^bot ^^and ^^this  ^^action ^^was ^^performed ^^automatically ^^for ^^questions ^^pm ^^me. ^^[Source](https://github.com/maumau35/2400gbot)""") % author.name
-                        comment.reply(message)
-                        with open("posts_repli_to.txt", "w") as f:
-                            for author.name in posts_repli_to:
-                                f.write(author.name + "\n")
+
+                        n=1
+                        if n==1:
+                            comment.reply(message)
+                            with open("posts_repli_to.txt", "w") as f:
+                                for author.name in posts_repli_to:
+                                    f.write(author.name + "\n")
+
+
+
+
 
 
 
@@ -71,18 +86,19 @@ ___
                 if author.name not in posts_repli_to and author.name not in done:
                     if comment.subreddit not in ["test","depression","suicidewatch"]:
                         comment.body=comment.body.split("\n")
-                        print"replying to {0}'s (I'm lonely) comment: {1}".format(comment.author, comment.body)
+                        print"replying to {0}'s (I'm sad ) comment: {1}".format(comment.author, comment.body)
                         posts_repli_to.append(author.name)
                         message=(""">I'm sad
 
 Here are a few funny [cat pictures](https://imgur.com/a/eqX4F) for you /u/%s, to cheer you up!
 ___
 ^^Hello, ^^I'm ^^a ^^bot ^^and ^^this  ^^action ^^was ^^performed ^^automatically ^^for ^^questions ^^pm ^^me. ^^[Source](https://github.com/maumau35/2400gbot)""") % author.name
-                    comment.reply(message)
-                    with open("posts_repli_to.txt", "w") as f:
-                        for author.name in posts_repli_to:
-                             f.write(author.name + "\n")
-
+                        n=1
+                        if n==1:
+                            comment.reply(message)
+                            with open("posts_repli_to.txt", "w") as f:
+                                for author.name in posts_repli_to:
+                                    f.write(author.name + "\n")
 
 
 
